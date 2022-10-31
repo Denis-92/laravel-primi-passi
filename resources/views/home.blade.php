399ed74f5825fa2b32df8f2472459ed02482623a
Denis-92 <d.mozenic@gmail.com>
2022-10-31 16:14:37 +0100

    add data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,31 @@
 </head>
 <body>
 
-<h1>HELLO WORLD</h1>
+    @php
+        $data = [
+            [
+                "name" => "pippo",
+                "age" => "45",
+            ],
+            [
+                "name" => "paperino",
+                "age" => "39",
+            ],
+        ];
+    @endphp
+
+    <h1>HELLO WORLD</h1>
+
+    <div>
+        @foreach ( $data as $item )
+
+            <div>
+                <span> {{ $item["name"] }} </span>
+                <span> {{ $item["age"] }} </span>
+            </div>
+
+        @endforeach
+    </div>
     
 </body>
 </html>
